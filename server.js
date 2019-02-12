9048e61962deff439e9043542cfe7e9e752960af
Joshua Moore <jommoore2003@me.com>
2019-02-12 00:41:35 -0800

    added mongodb_uri to server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ var request = require("request");
 // Require all models
 var db = require("./models");
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 // Initialize Express
 var PORT = process.env.PORT || 3000;
 
